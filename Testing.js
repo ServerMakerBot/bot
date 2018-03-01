f51837eed7a44ce1f95e67db488c7c4b1a52d495
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-03-01 18:56:40 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -11,7 +11,7 @@ bot.on('message', message => {
 	if(message.content.toLowerCase() === prefix + 'support') {
 		message.channel.send('support@mcpeservermaker.com')
 	}
-	if(message.conetent.toLowerCase() === prefix + 'permissions fly') {
+	if(message.conetent.toLowerCase() === prefix + 'permission fly') {
 		embed.setColor('RED');
 		embed.setDescription('Permission : essentials.fly\n**Usage**\n`/setuperm <user> essentials.fly`\n`/setgperm <group> essentials.fly`');
 		message.channel.send({embed})
