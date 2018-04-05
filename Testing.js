7025fd46e109ac9e943025fadb8015df387bbbfc
TMG / Michael <32754218+themaggiesgamers@users.noreply.github.com>
2018-04-05 16:19:45 -0400

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const fs = require('fs')
+const fs = require('fs');
 const config = require("./config.json");
 var prefix = config.prefix1
 
